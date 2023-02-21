8dbb7ef09da5237d1ec0ddcefd06f4c92ad69a37
Mikaelly Vitória <hermesonbastos@gmail.com>
2023-02-21 15:07:45 -0300

    primeiro commit

---
+++ variaveis/script.js
@@ -0,0 +1,19 @@
+// Declarar uma variável com o seu nome
+var nome = "Hermeson";
+
+// Declarar uma variável com a sua idade
+var idade = 19;
+
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+var comida;
+
+// Atribuir valor a sua comida favorita
+comida = "Camarão";
+
+// Declarar 5 variáveis diferentes sem valores
+var l1, 
+    l2,
+    l3,
+    l4,
+    l5;
