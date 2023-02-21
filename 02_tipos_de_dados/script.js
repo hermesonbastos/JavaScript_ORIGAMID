b8edf80b71a9cdd6a5458c8d25f5830685f6bd13
Mikaelly Vitória <hermesonbastos@gmail.com>
2023-02-21 18:01:01 -0300

    tipos de dados

---
+++ 02_tipos_de_dados/script.js
@@ -0,0 +1,20 @@
+// Declare uma variável contendo uma string
+var eu = "Hermeson Bastos";
+
+// Declare uma variável contendo um número dentro de uma string
+var euComNumero = "Hermeson 19";
+
+// Declare uma variável com a sua idade
+var idade = 19;
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+var nome = "Hermeson", sobrenome = "Bastos";
+nome = `${nome} ${sobrenome}`;
+console.log(nome);
+
+// Coloque a seguinte frase em uma variável: It's time
+var frase = "It's time";
+
+// Verifique o tipo da variável que contém o seu nome
+console.log(typeof nome);
