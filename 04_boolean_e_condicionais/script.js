73741551fac7a2d2b5ab73ef89bd87266241840b
Mikaelly Vitória <hermesonbastos@gmail.com>
2023-02-21 19:15:25 -0300

    boolean e condicionais

---
+++ 04_boolean_e_condicionais/script.js
@@ -0,0 +1,50 @@
+// Verifique se a sua idade é maior do que a de algum parente
+// Dependendo do resultado coloque no console 'É maior', 'É igual' ou 'É menor'
+var minhaIdade = 19;
+var idadeMae = 40;
+if(minhaIdade == idadeMae){
+  console.log("É igual");
+} else if(minhaIdade < idadeMae){
+  console.log("É menor");
+} else {
+  console.log("É maior");
+}
+
+// Qual valor é retornado na seguinte expressão?
+var expressao = (5 - 2) && (5 - ' ') && (5 - 2);
+console.log(3)
+
+// Verifique se as seguintes variáveis são Truthy ou Falsy
+var nome = 'Andre';
+var idade = 28;
+var possuiDoutorado = false;
+var empregoFuturo;
+var dinheiroNaConta = 0;
+console.log(!!nome, !!idade, !!possuiDoutorado, !!empregoFuturo, !!dinheiroNaConta);
+
+// Compare o total de habitantes do Brasil com China (valor em milhões)
+var brasil = 207;
+var china = 1340;
+if(brasil == china){
+  console.log("É igual");
+} else if(brasil < china){
+  console.log("É menor");
+} else {
+  console.log("É maior");
+}
+
+// O que irá aparecer no console?
+if(('Gato' === 'gato') && (5 > 2)) {
+  console.log('Verdadeiro');
+} else {
+  console.log('Falso');
+}
+console.log('Falso');
+
+// O que irá aparecer no console?
+if(('Gato' === 'gato') || (5 > 2)) {
+  console.log('Gato' && 'Cão');
+} else {
+  console.log('Falso');
+}
+console.log('Cão')
