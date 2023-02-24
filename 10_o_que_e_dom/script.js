7a979e2cede441bc3ef10edebbe314e0b9912738
Mikaelly Vitória <hermesonbastos@gmail.com>
2023-02-23 22:45:09 -0300

    o que é o DOM

---
+++ 10_o_que_e_dom/script.js
@@ -0,0 +1,13 @@
+// Retorne o url da página atual utilizando o objeto window
+console.log(window.location.href);
+
+// Seleciona o primeiro elemento da página que
+// possua a classe ativo
+const on = document.querySelector('.ativo');
+console.log(on);
+
+// Retorne a linguagem do navegador
+console.log(window.navigator.language);
+
+// Retorne a largura da janela 
+console.log(window.screen.width);
